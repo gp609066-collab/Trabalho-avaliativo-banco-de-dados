9303898a4171c18a490553b6cfec1d544c6da05a
Guilherme-2inf <gp609066@gmail.com>
2025-11-13 16:04:10 -0300

    Create Questão 8.sql

---
+++ Questão 8.sql
@@ -0,0 +1,10 @@
+CREATE VIEW vw_consultas_realizadas AS
+SELECT 
+    p.nome AS paciente,
+    m.nome AS medico,
+    m.especialidade,
+    c.valor,
+    c.data_consulta
+FROM Consulta c
+JOIN Paciente p ON c.id_paciente = p.id_paciente
+JOIN Medico m ON c.id_medico = m.id_medico;
