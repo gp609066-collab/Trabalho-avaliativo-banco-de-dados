a161fee8b405307dde23460c5b698d587f67d81b
Guilherme-2inf <gp609066@gmail.com>
2025-11-13 16:06:58 -0300

    Create Questão 11.sql

---
+++ Questão 11.sql
@@ -0,0 +1,7 @@
+INSERT INTO Pagamento (id_consulta, forma_pagamento, valor_pago, data_pagamento)
+VALUES
+(1,'Pix',200,'2025-11-01'),
+(3,'Dinheiro',220,'2025-11-03'),
+(5,'Cartao',250,'2025-11-05'),
+(7,'Boleto',180,'2025-11-07'),
+(9,'Pix',150,'2025-11-09');
